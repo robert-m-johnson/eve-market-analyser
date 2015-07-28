65889881f9b4ca816705f955ffccbd34490df130
Robert Johnson <robert.m.johnson88@gmail.com>
2015-07-28 21:02:29 +0100

    Initial commit

---
+++ test/eve_market_analyser_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns eve-market-analyser-clj.core-test
+  (:require [clojure.test :refer :all]
+            [eve-market-analyser-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
