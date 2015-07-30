004f7622456ae1eb08913b9dc386f6161b7b63b3
Robert Johnson <robert.m.johnson88@gmail.com>
2015-07-30 20:28:37 +0100

    Added receive timeout

--- src/eve_market_analyser_clj/core.clj
+++ src/eve_market_analyser_clj/core.clj
@@ -5,11 +5,10 @@
 (defn -main []
   (let [context (zmq/context 1)]
     (println "Connecting to hello world server…")
-    (with-open [socket (doto (zmq/socket context :req)
-                         (zmq/connect "tcp://127.0.0.1:5555"))]
+    (with-open [subscriber (doto (zmq/socket context :sub)
+                             (zmq/connect "tcp://relay-eu-germany-1.eve-emdr.com:8050")
+                             (zmq/set-receive-timeout 10))]
       (dotimes [i 10]
-        (let [request "Hello"]
-          (println "Sending hello " i "…")
-          (zmq/send-str socket request)
-          (zmq/receive socket)
-          (println "Received world " i))))))
+        (println "Receiving item...")
+        (zmq/receive subscriber)
+        (println "Received item ")))))
