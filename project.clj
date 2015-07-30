a53e2f37eeb020ecd0eecb07dda7f6de84d40cf8
Robert Johnson <robert.m.johnson88@gmail.com>
2015-07-30 19:53:02 +0100

    Added cljzmq dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.zeromq/cljzmq "0.1.4"]]
+  :jvm-opts ["-Djava.library.path=/usr/lib:/usr/local/lib"]
   :main ^:skip-aot eve-market-analyser-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/eve_market_analyser_clj/core.clj
+++ src/eve_market_analyser_clj/core.clj
@@ -1,7 +1,15 @@
 (ns eve-market-analyser-clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [zeromq.zmq :as zmq]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main []
+  (let [context (zmq/context 1)]
+    (println "Connecting to hello world server…")
+    (with-open [socket (doto (zmq/socket context :req)
+                         (zmq/connect "tcp://127.0.0.1:5555"))]
+      (dotimes [i 10]
+        (let [request "Hello"]
+          (println "Sending hello " i "…")
+          (zmq/send-str socket request)
+          (zmq/receive socket)
+          (println "Received world " i))))))
