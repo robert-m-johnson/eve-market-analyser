14b8da254a38256b839821af42a3915c7be42c87
Robert Johnson <robert.m.johnson88@gmail.com>
2015-08-01 11:26:06 +0100

    Added cheshire dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.zeromq/cljzmq "0.1.4"]]
+                 [org.zeromq/cljzmq "0.1.4"]
+                 ;; JSON parsing
+                 [cheshire "5.5.0"]]
   :jvm-opts ["-Djava.library.path=/usr/lib:/usr/local/lib"]
   :main ^:skip-aot eve-market-analyser-clj.core
   :target-path "target/%s"
