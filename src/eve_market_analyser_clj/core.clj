f6d324ac3d3ea1d43422d70048299dcef7108aea
Robert Johnson <robert.m.johnson88@gmail.com>
2015-12-28 20:41:49 +0000

    No supports no buy orders

--- src/eve_market_analyser_clj/core.clj
+++ src/eve_market_analyser_clj/core.clj
@@ -16,6 +16,12 @@
 (defn to-string [^bytes x]
   (String. x (Charset/forName "UTF-8")))
 
+(defn- apply-or-default [default f s]
+  (if (empty? s)
+    default
+    (apply f s)))
+
+
 (defn feed->region-item [feed-item]
   (let [col-names (:columns feed-item)
         price-index (.indexOf  col-names "price")
@@ -30,8 +36,8 @@
            (let [orders (->> (:rows rowset) (map order-vec->order))
                  buyOrders (->>  (filter #(:isBid %) orders) (map #(dissoc % :isBid)) (sort-by :price >))
                  sellOrders (->> (filter #(not (:isBid %)) orders) (map #(dissoc % :isBid)) (sort-by :price))
-                 buyingPrice (->> (map :price buyOrders) (apply max))
-                 sellingPrice (->> (map :price sellOrders) (apply min))]
+                 buyingPrice (->> (map :price buyOrders) (apply-or-default nil max))
+                 sellingPrice (->> (map :price sellOrders) (apply-or-default nil min))]
              {:generatedTime (:generatedAt rowset)
               :typeID (:typeID rowset)
               :itemName (world/types (:typeID rowset))
--- test/eve_market_analyser_clj/core_test.clj
+++ test/eve_market_analyser_clj/core_test.clj
@@ -98,3 +98,28 @@
               :sellOrders [{:price 8999 :quantity 1} {:price 11499.99 :quantity 10} {:price 11500 :quantity 50}]
               :buyOrders [{:price 6000 :quantity 50} {:price 5000 :quantity 50} {:price 4000 :quantity 50}]})
            (feed->region-item feed-item-with-unknown-region)))))
+
+(def feed-item-with-no-buy-orders
+  {:resultType "orders", :version "0.1", :uploadKeys [{:name "emk", :key "abc"} {:name "ec", :key "def"}],
+   :generator {:name "Yapeal", :version "11.335.1737"}, :currentTime "2011-10-22T15:46:00+00:00",
+   :columns ["price" "volRemaining" "range" "orderID" "volEntered" "minVolume" "bid" "issueDate" "duration" "stationID" "solarSystemID"],
+   :rowsets [
+             {:generatedAt "2011-10-22T15:43:00+00:00", :regionID 10000065, :typeID 11134,
+              :rows [
+                     [11500 48 32767 2363413004 50 1 false "2011-12-02T22:44:01+00:00" 90 60006967 30005039]
+                     [8999 1 32767 2363806077 1 1 false "2011-12-03T08:10:59+00:00" 90 60008692 30005038]
+                     [11499.99 10 32767 2363915657 10 1 false "2011-12-03T10:53:26+00:00" 90 60006970 nil]]}]}
+)
+
+(deftest no-buy-orders
+  (testing "No buy orders"
+    (is (= '({:generatedTime "2011-10-22T15:43:00+00:00"
+              :typeID 11134
+              :itemName "Amarr Shuttle"
+              :regionID 10000065
+              :regionName "Kor-Azor"
+              :sellingPrice 8999
+              :buyingPrice nil
+              :sellOrders [{:price 8999 :quantity 1} {:price 11499.99 :quantity 10} {:price 11500 :quantity 50}]
+              :buyOrders []})
+           (feed->region-item feed-item-with-no-buy-orders)))))
