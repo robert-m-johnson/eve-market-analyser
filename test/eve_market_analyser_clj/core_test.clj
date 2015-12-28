04cf97c23f1949e89a28adfaa7df8a78b6f77096
Robert Johnson <robert.m.johnson88@gmail.com>
2015-12-28 20:51:12 +0000

    Added test for no sell orders

--- test/eve_market_analyser_clj/core_test.clj
+++ test/eve_market_analyser_clj/core_test.clj
@@ -123,3 +123,27 @@
               :sellOrders [{:price 8999 :quantity 1} {:price 11499.99 :quantity 10} {:price 11500 :quantity 50}]
               :buyOrders []})
            (feed->region-item feed-item-with-no-buy-orders)))))
+
+(def feed-item-with-no-sell-orders
+  {:resultType "orders", :version "0.1", :uploadKeys [{:name "emk", :key "abc"} {:name "ec", :key "def"}],
+   :generator {:name "Yapeal", :version "11.335.1737"}, :currentTime "2011-10-22T15:46:00+00:00",
+   :columns ["price" "volRemaining" "range" "orderID" "volEntered" "minVolume" "bid" "issueDate" "duration" "stationID" "solarSystemID"],
+   :rowsets [
+             {:generatedAt "2011-10-22T15:43:00+00:00", :regionID 10000065, :typeID 11134,
+              :rows [[5000 48 32767 2363413004 50 1 true "2011-12-02T22:44:01+00:00" 90 60006967 30005039]
+                     [6000 48 32767 2363413004 50 1 true "2011-12-02T22:44:01+00:00" 90 60006967 30005039]
+                     [4000 48 32767 2363413004 50 1 true "2011-12-02T22:44:01+00:00" 90 60006967 30005039]]}]}
+)
+
+(deftest no-sell-orders
+  (testing "No sell orders"
+    (is (= '({:generatedTime "2011-10-22T15:43:00+00:00"
+              :typeID 11134
+              :itemName "Amarr Shuttle"
+              :regionID 10000065
+              :regionName "Kor-Azor"
+              :sellingPrice nil
+              :buyingPrice 6000
+              :sellOrders []
+              :buyOrders [{:price 6000 :quantity 50} {:price 5000 :quantity 50} {:price 4000 :quantity 50}]})
+           (feed->region-item feed-item-with-no-sell-orders)))))
