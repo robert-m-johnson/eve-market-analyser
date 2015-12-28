49f5f990ce9af3e779fef57e81af14d72ff3cb3d
Robert Johnson <robert.m.johnson88@gmail.com>
2015-12-28 23:32:45 +0000

    Order item contruction is now done using HOF

--- src/eve_market_analyser_clj/core.clj
+++ src/eve_market_analyser_clj/core.clj
@@ -21,24 +21,33 @@
     default
     (apply f s)))
 
+(defn- order-vec->order* [{col-names :columns :as feed-item}
+                          name-map]
+    (fn [order-vec]
+      (->>
+       (map
+        (fn [entry]
+          (let [k (key entry)
+                n (val entry)
+                index (.indexOf col-names n)]
+            [k (nth order-vec index)]))
+        name-map)
+       flatten
+       (apply  hash-map))))
 
 (defn feed->region-item [feed-item]
-  (let [col-names (:columns feed-item)
-        price-index (.indexOf  col-names "price")
-        bid-index (.indexOf  col-names "bid")
-        quantity-index (.indexOf col-names "volEntered")
-        order-vec->order (fn [order-vec]
-                           {:price (nth order-vec price-index)
-                            :isBid (nth order-vec bid-index)
-                            :quantity (nth order-vec quantity-index)})
-        rowsets (->>
-                 (:rowsets feed-item)
+  (let [order-vec->order (order-vec->order*
+                          feed-item
+                          {:price "price"
+                           :quantity "volEntered"
+                           :isBid "bid"})
+        rowsets (->> (:rowsets feed-item)
                  (filter :regionID) ;; Filter items with no region ID
                  (filter :typeID) ;; Filter items with no type ID
                  )]
     (map (fn [rowset]
            (let [orders (->> (:rows rowset) (map order-vec->order))
-                 buyOrders (->>  (filter #(:isBid %) orders) (map #(dissoc % :isBid)) (sort-by :price >))
+                 buyOrders (->>  (filter :isBid orders) (map #(dissoc % :isBid)) (sort-by :price >))
                  sellOrders (->> (filter #(not (:isBid %)) orders) (map #(dissoc % :isBid)) (sort-by :price))
                  buyingPrice (->> (map :price buyOrders) (apply-or-default nil max))
                  sellingPrice (->> (map :price sellOrders) (apply-or-default nil min))]
