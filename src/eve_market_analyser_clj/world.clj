6639213b4053d2824bbd762897181b4ba5d4587d
Robert Johnson <robert.m.johnson88@gmail.com>
2016-02-23 22:54:18 +0000

    Added docstring to types map

--- src/eve_market_analyser_clj/world.clj
+++ src/eve_market_analyser_clj/world.clj
@@ -2,17 +2,19 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def types (with-open [rdr (io/reader (io/resource "types.csv"))]
-             (let [lines (line-seq rdr)
-                   pairs (map
-                          (fn [line]
-                            ;; Split the CSV lines by the pipe | symbol
-                            (let [[id-str name] (str/split line #"\|")]
-                              ;; Parse the ID string into an int
-                              [(java.lang.Integer/parseInt id-str) name]))
-                          lines)]
-               ;; Create a map of the ID-name pairs
-               (->> pairs flatten (apply hash-map)))))
+(def types
+  "A map of the type IDs and their corresponding item names"
+  (with-open [rdr (io/reader (io/resource "types.csv"))]
+    (let [lines (line-seq rdr)
+          pairs (map
+                 (fn [line]
+                   ;; Split the CSV lines by the pipe | symbol
+                   (let [[id-str name] (str/split line #"\|")]
+                     ;; Parse the ID string into an int
+                     [(java.lang.Integer/parseInt id-str) name]))
+                 lines)]
+      ;; Create a map of the ID-name pairs
+      (->> pairs flatten (apply hash-map)))))
 
 (def regions
   "A map of the region IDs and their corresponding names"
